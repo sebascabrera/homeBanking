d676a10bc70f815cab60e614d972a40c2a7be1d8
Sebastian <sebasfedele@gmail.com>
2023-08-06 23:45:32 -0300

    import java.util.List; in ClientRepository /

--- src/main/java/com/apmindhub/homeBankingmh/repository/ClientRepository.java
+++ src/main/java/com/apmindhub/homeBankingmh/repository/ClientRepository.java
@@ -4,8 +4,11 @@ import com.apmindhub.homeBankingmh.models.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface ClientRepository extends JpaRepository<Client,Long> {
 
-
+    List<Client> findAllByLastName(String lastName);
+    /*.orElse(null) Se puede agregar??*/
 }
