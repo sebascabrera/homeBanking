375dafc7e6f0d15c57f448ceaeda479b71235edb
Sebastian <sebasfedele@gmail.com>
2023-08-17 21:44:29 -0300

    statics

--- src/main/resources/static/web/js/accounts.js
+++ src/main/resources/static/web/js/accounts.js
@@ -1,32 +1,32 @@
-Vue.createApp({
+const { createApp } = Vue;
+
+createApp({
     data() {
         return {
             clientInfo: {},
             errorToats: null,
             errorMsg: null,
-        };
+        }
     },
     methods: {
         getData() {
-            const urlParams = new URLSearchParams(window.location.search);
-            const id = urlParams.get('id');
-            axios.get(`/api/clients/${id}`)
+            axios.get("/api/clients/1")
                 .then((response) => {
-                    // Obtener la información del cliente
+                    //get client ifo
                     this.clientInfo = response.data;
                 })
                 .catch((error) => {
-                    // Manejar el error
+                    // handle error
                     this.errorMsg = "Error getting data";
                     this.errorToats.show();
-                });
+                })
         },
         formatDate(date) {
-            return new Date(date).toLocaleDateString('en-us');
-        },
+            return new Date(date).toLocaleDateString('en-gb');
+        }
     },
     mounted() {
         this.errorToats = new bootstrap.Toast(document.getElementById('danger-toast'));
         this.getData();
-    },
+    }
 }).mount('#app');
