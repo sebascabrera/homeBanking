00af071934dd673ad3c253da003abad49ae5aeb8
Sebastian <sebasfedele@gmail.com>
2023-09-11 21:50:41 -0300

    LoanController

---
+++ src/main/java/com/apmindhub/homeBankingmh/controllers/LoanController.java
@@ -0,0 +1,10 @@
+package com.apmindhub.homeBankingmh.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class LoanController {
+
+}
